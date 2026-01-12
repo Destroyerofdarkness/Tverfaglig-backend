6341e3e933d659d8fc58902f7f5430379b7f8400
Destroyerofdarkness <dawidadogier@gmail.com>
2026-01-12 14:37:29 +0100

    Rendered in a random qoute to the homepage

--- controller/default_controller.js
+++ controller/default_controller.js
@@ -1,8 +1,15 @@
+const Quote = require("../models/Quote.js")
 
+const randomQoute = (list)=>{
+    const random = Math.floor(Math.random()*list.length)
+    return list[random]
+}
 
-const render_home = (req,res)=>{
+const render_home = async(req,res)=>{
     try{
-        res.render("index")
+        const quoteList = await Quote.find()
+        const quote = randomQoute(quoteList)
+        res.render("index", {quote})
     }catch(err){
         res.status(500).send(err)
     }
