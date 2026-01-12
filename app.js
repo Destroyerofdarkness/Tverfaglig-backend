403ea2fd8e2734f98e43b53f246be00511ab6555
Destroyerofdarkness <dawidadogier@gmail.com>
2026-01-12 13:26:45 +0100

    Made a vm for mongodb and connected the server to the database

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require("express");
 
 const app = express();
 
+const {connectToMongoDb}= require("./handlers/mongoDbHandler.js")
+
+require("dotenv").config();
+
 app.set("view engine", "ejs");
 
 app.use(express.json());
 
 app.set(express.urlencoded({extended:true}));
 
+connectToMongoDb(process.env.mongodb)
+
 const main_router = require("./routes/default_router.js")
 
 app.use(main_router)
 
-app.listen(3000, ()=>{
+app.listen(3000, async()=>{
     console.log("Server running on port 3000");
 });
